5401b315721d93057485c6dce0d8602c729d1711
lucasbuttow <lucas_buttow@outlook.com>
2019-10-17 21:28:42 -0300

    funcionalidade de filtro por tag

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,10 +6,26 @@ const Post = mongoose.model('Post');
 exports.index = async(req, res) => {
   let responseJson = {
     'pageTitle': 'HOME',
-    posts:[]
+    posts:[],
+    tags:[],
+    tag:''
   };
 
-  const posts = await Post.find();
+  responseJson.tag = req.query.t;
+  const postFilter = (typeof responseJson.tag != 'undefined') ? { tags:responseJson.tag }: {};
+
+  const tagsPromise =  Post.getTagsList();
+  const postsPromise =  Post.find(postFilter);
+
+  const [ tags, posts ] = await Promise.all([tagsPromise, postsPromise]);
+
+  for(let i in tags) {
+    if(tags[i]._id == responseJson.tag) {
+      tags[i].class = "selected";
+    }
+  }
+  responseJson.tags = tags;
   responseJson.posts = posts;
+
   res.render('home', responseJson);
 }
--- models/Post.js
+++ models/Post.js
@@ -18,12 +18,29 @@ const postSchema = new mongoose.Schema({
   tags:[String]
 });
 
-postSchema.pre('save', function(next) {
+postSchema.pre('save', async function(next) {
   if(this.isModified('title')) {
     this.slug = slug( this.title, {lower:true} );
+
+    const slugRegex = new RegExp(`^(${this.slug})((-[0-9]{1,})?)$`, 'i');
+
+    const postsWithSlug = await this.constructor.find({slug:slugRegex});
+
+    if(postsWithSlug.length > 0) {
+      this.slug = `${this.slug}-${postsWithSlug.length + 1}`;
+    }
+
   }
 
   next();
 });
+
+postSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind:'$tags' },
+    { $group:{ _id:'$tags', count:{ $sum:1 } } },
+    { $sort:{ count:-1 } }
+  ]);
+}
 
 module.exports = mongoose.model('Post', postSchema);
